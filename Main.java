0585ba74fc879c8285a89efd9a911e0a7442a3fa
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2022-04-09 01:38:50 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int total = 0;
-    int gradeCounter=0;
-    System.out.print("Enter grade or -1 to quit: ");
-    int grade = input.nextInt();
-    while(grade != -1){
-      total= total+grade;
-      gradeCounter=gradeCounter+1;
-      System.out.print("Enter grade or -1 to quit: ");
-      grade=input.nextInt();
-    }
-    if (gradeCounter !=0){
-      double average = (double) total/gradeCounter;
-      System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter,total);
-      System.out.printf("Class avarage is %.2f%n",average);
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner tem1 = new Scanner(System.in);
+		Scanner tem2 = new Scanner(System.in);
+		Scanner tem3 = new Scanner(System.in);
+    Scanner tem4 = new Scanner(System.in);
+
+		int decision=0;
+    while (decision<2){
       
-    }
-    else{
-      System.out.println("No grades were entered");
+      System.out.print("Enter the minimum tempereture in degrees  Celsius : ");
+		  double in_tem1 =tem1.nextInt();
+		  System.out.print("Enter the maximum tempereture in degrees  Celsius : ");
+		  double in_tem2 =tem2.nextInt();
+		  System.out.print("Enter the values of a range of temperatur : ");
+		  double in_tem3 =tem3.nextInt();
+		  System.out.println("° Celsius" +"			"+"° Fahrenheit");
+
+      for (double i= in_tem1; i<= in_tem2; i=i+in_tem3 ) {
+			  double f;
+			  f=((9.0/5.0)*i)+32;
+			  System.out.println(" "+ i+ "		=		"+f);
+		}
+      System.out.println("If you want to continue the program then Type: y or not type n : ");
+			char disision_1 = tem4.nextLine().charAt(0);
+			if (disision_1=='n') {
+				decision=2;
+				System.out.println("..................Work Done.................");
+      }
     }
   }
 }
